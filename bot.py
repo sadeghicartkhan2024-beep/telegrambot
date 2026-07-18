2db1c79aeeeceba436192ad5ead4fd0fea539289
sadeghicartkhan2024-beep <sadeghicartkhan2024@gmail.com>
2026-07-18 05:45:30 +0330

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,28 @@
+from telegram.ext import Updater, CommandHandler
+
+TOKEN = "8616468391:AAESytyDpnLW9BdKY1up9qc71X8tdbWyeg"
+
+def start(update, context):
+    update.message.reply_text(
+        """سلام 👋
+
+به ربات شرکت نگین تمدن پاسارگاد خوش آمدید.
+
+1- معرفی شرکت
+2- محصولات
+3- استعلام قیمت
+4- ارتباط با کارشناس
+5- آدرس شرکت
+"""
+    )
+
+updater = Updater(TOKEN, use_context=True)
+
+dp = updater.dispatcher
+
+dp.add_handler(CommandHandler("start", start))
+
+print("Bot Started...")
+
+updater.start_polling()
+updater.idle()
